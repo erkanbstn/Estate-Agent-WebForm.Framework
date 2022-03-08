312006f94313155403a3a3fbdc0470fa7edb0fc9
Erkan Bostan <profesyonel59@gmail.com>
2022-03-08 05:13:48 +0300

    Kurumsal Emlak Sitesi
    ASP.Net Kurumsal Emlak Sitesi Projesi

---
+++ YaziciEmlak/YaziciEmlak/Entity/Baglanti.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace YaziciEmlak.Entity
+{
+    public class Baglanti
+    {
+        public static YaziciDBEntities db = new YaziciDBEntities();
+
+        public static SqlConnection bgl = new SqlConnection("Data Source=GEOPC\\SQLEXPRESS;Initial Catalog=YaziciDB;Integrated Security=True;");
+    }
+}
---
+++ YaziciEmlak/YaziciEmlak/YeniIlanEkle.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminPanel.Master" AutoEventWireup="true" CodeBehind="YeniIlanEkle.aspx.cs" Inherits="YaziciEmlak.YeniIlanEkle" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
